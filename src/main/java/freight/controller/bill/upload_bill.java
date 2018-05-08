869b392cc16d4cb32fc672e12d0f43d30cdc5fc4
Tiancityrua <ztc_1995@126.com>
2018-05-08 22:28:28 +0800

    OCR基本完成

---
+++ src/main/java/freight/controller/bill/upload_bill.java
@@ -0,0 +1,81 @@
+package freight.controller.bill;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>创建时间: 2018/5/8</p>
+ * <p>作者：tianchang</p>
+ * ====================================================================<br>
+ */
+@Controller
+public class upload_bill {
+    @ResponseBody
+    @RequestMapping(value = "/upload/bill/hawb",method = RequestMethod.POST,produces = "application/json")
+    public Map hawb_up(@RequestParam("file") CommonsMultipartFile file, HttpServletRequest request) {
+        String path = request.getSession().getServletContext().getRealPath("/") + file.getOriginalFilename();
+        File newfile = new File(path);
+        try {
+            file.transferTo(newfile);
+            ITesseract tesseract=new Tesseract();
+            tesseract.setPageSegMode(4);
+            String result=tesseract.doOCR(newfile);
+            String arr[]=result.split("\n\n");
+            Map resultmap=new HashMap();
+            Map returnmap=new HashMap<String,Map>();
+            resultmap.put("mawb",arr[0]);
+            resultmap.put("hawb",arr[1]);
+            resultmap.put("airdeparture",arr[2]);
+            resultmap.put("to1",arr[3]);
+            resultmap.put("by1",arr[4]);
+            resultmap.put("to2",arr[5]);
+            resultmap.put("airdest",arr[6]);
+            resultmap.put("flightno",arr[7]);
+            resultmap.put("currency",arr[8]);
+            resultmap.put("declaredcarriage",arr[9]);
+            resultmap.put("declaredcustoms",arr[10]);
+            resultmap.put("amountinsurance",arr[11]);
+            resultmap.put("handlinginfo",arr[12]);
+            resultmap.put("accountinfo",arr[13]);
+            resultmap.put("shipper",arr[14]);
+            String consignee="";
+            for(int i=15;i<arr.length;i++){
+                consignee=consignee+arr[i];
+            }
+            resultmap.put("consignee",consignee);
+            returnmap.put("data",resultmap);
+            returnmap.put("msg","success");
+            return returnmap;
+        } catch (IOException e) {
+            Map returnmap=new HashMap();
+            returnmap.put("msg","error");
+            returnmap.put("event","上传错误");
+            return returnmap;
+        } catch (TesseractException e) {
+            Map returnmap=new HashMap();
+            returnmap.put("msg","error");
+            returnmap.put("event","解析错误");
+            return returnmap;
+        }
+    }
+//    @ResponseBody
+//    @RequestMapping(value = "/upload/bill/mawb",method = RequestMethod.POST,produces = "application/json")
+//    public Map mawb_up(@RequestParam("file") CommonsMultipartFile file, HttpServletRequest request) {
+//        String path = request.getSession().getServletContext().getRealPath("/") + file.getOriginalFilename();
+//        File newfile = new File(path);
+//
+//    }
+}
--- src/main/java/freight/controller/invoice/upload_invoice.java
+++ src/main/java/freight/controller/invoice/upload_invoice.java
@@ -33,14 +33,26 @@ public class upload_invoice {
             String result=tesseract.doOCR(newfile);
             String arr[]=result.split("\n\n");
             Map resultmap=new HashMap();
-            resultmap.put("invoiceTo",arr[0]);
-            resultmap.put("invoiceNo",arr[1]);
-            resultmap.put("date",arr[2]);
-            resultmap.put("billLaden",arr[3]);
-            resultmap.put("origin",arr[4]);
-            resultmap.put("dstn",arr[5]);
-            resultmap.put("nature",arr[6]);
             Map returnmap=new HashMap<String,Map>();
+            if(arr.length==7) {
+                resultmap.put("invoiceTo", arr[0]);
+                resultmap.put("invoiceNo", arr[1]);
+                resultmap.put("date", arr[2]);
+                resultmap.put("billLaden", arr[3]);
+                resultmap.put("origin", arr[4]);
+                resultmap.put("dstn", arr[5]);
+                resultmap.put("nature", arr[6]);
+            }
+            else {
+                resultmap.put("invoiceTo", arr[0]);
+                resultmap.put("invoiceNo", arr[1]);
+                resultmap.put("date",arr[2]);
+                resultmap.put("terms",arr[3]);
+                resultmap.put("billLaden", arr[4]);
+                resultmap.put("origin", arr[5]);
+                resultmap.put("dstn", arr[6]);
+                resultmap.put("nature", arr[7]);
+            }
             returnmap.put("data",resultmap);
             returnmap.put("msg","success");
             return returnmap;
@@ -56,4 +68,59 @@ public class upload_invoice {
             return returnmap;
         }
     }
+    @ResponseBody
+    @RequestMapping(value = "/upload/invoice/note",method = RequestMethod.POST,produces = "application/json")
+    public Map note_up(@RequestParam("file") CommonsMultipartFile file, HttpServletRequest request){
+        String path=request.getSession().getServletContext().getRealPath("/")+file.getOriginalFilename();
+        File newfile=new File(path);
+        try {
+            file.transferTo(newfile);
+            ITesseract tesseract=new Tesseract();
+            tesseract.setPageSegMode(4);
+            String result=tesseract.doOCR(newfile);
+            String arr[]=result.split("\n\n");
+            Map resultmap=new HashMap();
+            Map returnmap=new HashMap<String,Map>();
+            if(arr[1].indexOf("-")!=-1){
+                resultmap.put("date",arr[0]);
+                resultmap.put("hawb",arr[1]);
+                resultmap.put("pieces",arr[2]);
+                resultmap.put("weight",arr[3]);
+                String add="";
+                for(int i=4;i<arr.length;i++){
+                    add=add+arr[i];
+                }
+                resultmap.put("add",add);
+            }
+            else {
+                resultmap.put("name",arr[0]);
+                resultmap.put("signed",arr[1]);
+                resultmap.put("dated",arr[2]);
+                resultmap.put("date",arr[3]);
+                resultmap.put("hawb",arr[4]);
+                resultmap.put("pieces",arr[5]);
+                resultmap.put("weight",arr[6]);
+                String add="";
+                for(int i=7;i<arr.length;i++){
+                    add=add+arr[i];
+                }
+                resultmap.put("add",add);
+            }
+            returnmap.put("data",resultmap);
+            returnmap.put("msg","success");
+            return returnmap;
+        } catch (IOException e) {
+            Map returnmap=new HashMap();
+            returnmap.put("msg","error");
+            returnmap.put("event","上传错误");
+            return returnmap;
+        } catch (TesseractException e) {
+            Map returnmap=new HashMap();
+            returnmap.put("msg","error");
+            returnmap.put("event","解析错误");
+            return returnmap;
+        }
+
+    }
+
 }
