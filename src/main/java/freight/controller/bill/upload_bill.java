32190d513f2cb4b45cd4cadfc9bf28d5086959cf
Tiancityrua <ztc_1995@126.com>
2018-05-09 20:03:20 +0800

    OCR全部完成

--- src/main/java/freight/controller/bill/upload_bill.java
+++ src/main/java/freight/controller/bill/upload_bill.java
@@ -71,11 +71,49 @@ public class upload_bill {
             return returnmap;
         }
     }
-//    @ResponseBody
-//    @RequestMapping(value = "/upload/bill/mawb",method = RequestMethod.POST,produces = "application/json")
-//    public Map mawb_up(@RequestParam("file") CommonsMultipartFile file, HttpServletRequest request) {
-//        String path = request.getSession().getServletContext().getRealPath("/") + file.getOriginalFilename();
-//        File newfile = new File(path);
-//
-//    }
+    @ResponseBody
+    @RequestMapping(value = "/upload/bill/mawb",method = RequestMethod.POST,produces = "application/json")
+    public Map mawb_up(@RequestParam("file") CommonsMultipartFile file, HttpServletRequest request) {
+        String path = request.getSession().getServletContext().getRealPath("/") + file.getOriginalFilename();
+        File newfile = new File(path);
+        try {
+            file.transferTo(newfile);
+            ITesseract tesseract=new Tesseract();
+            tesseract.setPageSegMode(4);
+            String result=tesseract.doOCR(newfile);
+            String arr[]=result.split("\n\n");
+            Map resultmap=new HashMap();
+            Map returnmap=new HashMap<String,Map>();
+            resultmap.put("airdeparture",arr[0]);
+            resultmap.put("to1",arr[1]);
+            resultmap.put("to2",arr[2]);
+            resultmap.put("airdest",arr[3]);
+            resultmap.put("flightno",arr[4]);
+            resultmap.put("currency",arr[5]);
+            resultmap.put("valcarriage",arr[6]);
+            resultmap.put("decalval",arr[7]);
+            resultmap.put("amountinsurance",arr[8]);
+            resultmap.put("handlinginfo",arr[9]);
+            resultmap.put("accountinfo",arr[10]);
+            resultmap.put("shipper",arr[11]);
+            String consignee="";
+            for(int i=12;i<arr.length;i++){
+                consignee=consignee+arr[i];
+            }
+            resultmap.put("consignee",consignee);
+            returnmap.put("data",resultmap);
+            returnmap.put("msg","success");
+            return returnmap;
+        } catch (IOException e) {
+            Map returnmap=new HashMap();
+            returnmap.put("msg","error");
+            returnmap.put("event","上传错误");
+            return returnmap;
+        } catch (TesseractException e) {
+            Map returnmap=new HashMap();
+            returnmap.put("msg","error");
+            returnmap.put("event","解析错误");
+            return returnmap;
+        }
+    }
 }
--- src/main/java/freight/tess.java
+++ src/main/java/freight/tess.java
@@ -1,10 +1,16 @@
 package freight;
 
-import net.sourceforge.tess4j.ITesseract;
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
 
-import java.io.File;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
 
 /**
  * <p>创建时间: 2018/5/5</p>
@@ -12,24 +18,25 @@ import java.io.File;
  * ====================================================================<br>
  */
 public class tess {
-    public static void  main(String[] args){
-        File image=new File("src/main/1.txt");
-        String result="GT Global Distributors\n" +
-                "9 Rugby Road, Saint Thamas, Exeter\n" +
-                "Devon, EX4 1BH\n" +
-                "\n" +
-                "INV518111501\n" +
-                "\n" +
-                "23/11/2015\n" +
-                "\n" +
-                "988-067/97¢\n" +
-                "\n" +
-                "SHA\n" +
-                "\n" +
-                "LHR\n" +
-                "\n" +
-                "1? pcs, 185 kg, Eyemarks";
-        String arr[]=result.split("\n\n");
-        System.out.println(arr);
+    public static void  main(String[] args) throws IOException {
+        File ex=new File("src/main/invoice.xlsx");
+//        POIFSFileSystem fs=new POIFSFileSystem(new FileInputStream(ex));
+//        HSSFWorkbook wb=new HSSFWorkbook(fs);
+//        HSSFSheet sheet=wb.getSheetAt(0);
+//        HSSFCell cell1=sheet.getRow(9).getCell(3);
+//        cell1.setCellValue(222);
+//        HSSFCell cell2=sheet.getRow(18).getCell(2);
+//        cell2.setCellValue(222222);
+//        FileOutputStream out=new  FileOutputStream("src/main/invoice1.xls");
+//        wb.write(out);
+//        out.close();
+        InputStream in=new FileInputStream(ex);
+        XSSFWorkbook wb=new XSSFWorkbook(in);
+        XSSFSheet sheet=wb.getSheetAt(0);
+        XSSFCell cell = sheet.getRow(8).getCell(2);
+        cell.setCellValue("qqqqqqqq");
+        FileOutputStream out=new FileOutputStream("src/main/invoice1.xlsx");
+        wb.write(out);
+        out.close();
     }
 }
